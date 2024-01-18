2f7da4bd8b959a8cc87165ce73c297bb2619153d
noblelar <59696875+noblelar@users.noreply.github.com>
2024-01-18 00:47:44 +0000

    r_hirehall_host

--- next.config.js
+++ next.config.js
@@ -2,14 +2,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // output: 'export',
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com'
-      }
-    ]
-  }
+  
 }
 
 module.exports = nextConfig
