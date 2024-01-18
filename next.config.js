ddab9cc38308c034682f38ce1acac38e78b033f8
noblelar <59696875+noblelar@users.noreply.github.com>
2024-01-18 00:41:38 +0000

    r_hirehall_host

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
  */
 const nextConfig = {
   // output: 'export',
- reactStrictMode: true,
+//  reactStrictMode: true,
 
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
